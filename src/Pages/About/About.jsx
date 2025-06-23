41eb919407081941dfd5c5a1690410ef5e624362
ahmeddadel502 <ahmeddadel502@gmail.com>
2025-06-23 17:22:37 +0300

    padding update

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -12,9 +12,9 @@ export default function About({toggleTheme,theme}) {
             <div className="flex  items-center justify-center mb-6">
                 <p className="text-white text-xl relative before:absolute before:w-[80px] before:h-[4px] before:bg-white before:left-[40px] before:top-[9px] after:absolute after:w-[80px] after:h-[4px] after:bg-white after:right-[40px] after:top-[9px]"><FaStar /></p>
             </div>
-            <div className="text-white flex justify-center gap-4">
-                <p className="sm:w-[40%] w-full px-4 text-left">Freelancer is a free bootstrap theme created by Route. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional SASS stylesheets for easy customization.</p>
-                <p className="sm:w-[40%] w-full px-4 text-left">Freelancer is a free bootstrap theme created by Route. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional SASS stylesheets for easy customization.</p>
+            <div className="text-white flex justify-center flex-col sm:flex-row gap-4">
+                <p className="sm:w-[40%] w-[80%] mx-auto px-4 text-left">Freelancer is a free bootstrap theme created by Route. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional SASS stylesheets for easy customization.</p>
+                <p className="sm:w-[40%] w-[80%] mx-auto px-4 text-left">Freelancer is a free bootstrap theme created by Route. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional SASS stylesheets for easy customization.</p>
             </div>
           </div>
    
